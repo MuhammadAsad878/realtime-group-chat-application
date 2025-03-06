928860fcbd53af7a59d14f51443c08eb6638a1e8
muhammadasad878 <muhammadasad4041@gmail.com>
2025-03-06 11:15:00 +0500

    All working

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -1,13 +1,13 @@
+// MODULES || Libraries 
 import express from "express";
 import cors from "cors";
 import { Server } from "socket.io";
-import mongoose from "mongoose";
-import { Message, MessageSchema } from "./models/message.js";
+import { Message,  } from "./models/message.js";
 import connectDb from "./database.js";
 import http from 'http';
 import fs from "fs";
 
-
+// Server Creation with express, http and WebSocket.io
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -18,34 +18,45 @@ const io = new Server(server, {
   }
 });
 
-
+// Middlewares 
 app.use(cors());
 app.use(express.json());
 
+// Connect DB
 connectDb();
 
+const filePath = './messages.json';
 
-function saveMsgToFile(message){
-  const filePath = './messages.json';
-  let messages = [];
 
-  if (fs.existsSync(filePath)) {
-      messages = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+function getMessagesFromFile(){
+  try{
+    if(fs.existsSync(filePath)){
+      const data = fs.readFileSync(filePath, 'utf-8');
+      if(data){
+      return JSON.parse(data).sort((a,b)=> a.timeStamp - b.timeStamp);
+      }
+    }
+  }catch(err){
+    console.log("Error reading files ", err);
   }
-  messages.push(message);
-  fs.writeFileSync(filePath, JSON.stringify(messages, null, 2));
-};
+  return null;
+}
+
+io.on('connection', async (socket) => {
+  console.log(`User with ID : ${socket.id} CONNECTED`);
 
 
-io.on('connection', (socket) => {
-  console.log('🟢 A user connected:', socket.id);
+  let messages = getMessagesFromFile();
 
-  // Load chat history
-  Message.find().sort({ timeStamp: 1 }).then(messages => {
-    console.log(messages);
-      socket.emit('chatHistory', messages);
-  });
+  if (!messages.length) {
+    messages = await Message.find().sort({ timeStamp: 1 });
+  }
+    
+  io.emit("chatHistory", messages); // Broadcast to all users
 
+  
+ 
   // Listen for new messages
   socket.on('newMessage', async (data) => {
     console.log("Data to be saved : ",data);
@@ -71,3 +82,16 @@ server.listen(port, () => {
   console.log("Server started on =>",`http://localhost:${port}`);
 
 });
+
+
+// Implemented function to also save messages in  /messages.json file
+function saveMsgToFile(message){
+  let messages = [];
+
+  if (fs.existsSync(filePath)) {
+      messages = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      console.log(messages, "Before after file save");
+  }
+  messages.push(message);
+  fs.writeFileSync(filePath, JSON.stringify(messages, null, 2));
+};
